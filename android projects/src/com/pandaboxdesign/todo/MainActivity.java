a398ab6283b75d47dad1c722a2f93548a16ee921
Jiang Tao Hong <jhong@pandaboxdesign.com>
2012-10-29 23:07:09 -0400

    implemented checkboxes

--- android projects/src/com/pandaboxdesign/todo/MainActivity.java
+++ android projects/src/com/pandaboxdesign/todo/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -17,21 +18,22 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity{
 
 	ArrayList<String> taskList = new ArrayList<String>();
 	ArrayAdapter<String> taskAdapter;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		taskAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, taskList);
+		taskAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, taskList);
 		ListView myList = (ListView)findViewById(R.id.toDos);
 		myList.setAdapter(taskAdapter);
 		EditText editText = (EditText) findViewById(R.id.add_here);
-		editText.setOnClickListener(this);
+		editText.setOnClickListener(sendListener);
 	}
 	
 	@Override
@@ -41,44 +43,43 @@ public class MainActivity extends Activity implements OnClickListener{
 	}
 	
 	/**
-	 * Called when user finishes adding a task
+	 * Called when user finishes adding a task and clicks send.
 	 */
-	public void onClick(View v){
-		EditText editText = (EditText) findViewById(R.id.add_here);
-		editText.setOnEditorActionListener(new OnEditorActionListener() {
-		    public boolean onEditorAction(TextView v, int actionId,
-					KeyEvent event) {
-		    	EditText editText = (EditText) findViewById(R.id.add_here);
-				if (actionId == EditorInfo.IME_ACTION_SEND){
-			    	String newTask = editText.getText().toString();
-			    	if (!(newTask.isEmpty())){
-				    	taskList.add(newTask);
-				    	taskAdapter.notifyDataSetChanged();
-				    	editText.setText(null);
-				    	return true;
-			    	}
+	private OnClickListener sendListener = new OnClickListener(){
+		public void onClick(View v){
+			EditText editText = (EditText) findViewById(R.id.add_here);
+			editText.setOnEditorActionListener(new OnEditorActionListener() {
+			    public boolean onEditorAction(TextView v, int actionId,
+						KeyEvent event) {
+			    	EditText editText = (EditText) findViewById(R.id.add_here);
+					if (actionId == EditorInfo.IME_ACTION_SEND){
+				    	String newTask = editText.getText().toString();
+				    	if (!(newTask.isEmpty())){
+					    	taskList.add(newTask);
+					    	taskAdapter.notifyDataSetChanged();
+					    	editText.setText(null);
+					    	return true;
+				    	}
+					}
+					return false;
 				}
-				return false;
-			}
-		});
-	}
-	
-
-	public void addToDo(View view) {
-		EditText editText = (EditText) findViewById(R.id.add_here);
-    	String newTask = editText.getText().toString();
-    	if (!(newTask.isEmpty())){
-	    	taskList.add(newTask);
-	    	taskAdapter.notifyDataSetChanged();
-	    	editText.setText(null);
-    	}
-	}
+			});
+		}
+	};
 	
 	/**
 	 * Called when user clicks Finish
 	 */
 	public void clearDone(View view){
-		taskList.clear();
+		ListView myList = (ListView)findViewById(R.id.toDos);
+		SparseBooleanArray checkedItemPositions = myList.getCheckedItemPositions();
+		int itemCount = myList.getCount();
+		for (int i=itemCount-1; i>=0; i--){
+			if (checkedItemPositions.get(i)){
+				myList.setItemChecked(i, false);
+				taskAdapter.remove(taskList.get(i));
+			}
+		}
 		taskAdapter.notifyDataSetChanged();
 	}
 }
